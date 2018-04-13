ab5869f6f41d61f784f4a3b2f9e4a6128ee03f4f
elizondo <mamami1991@hotmail.com>
2018-04-14 03:35:46 +0800

    利用element大致布局

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 ## 1.开发环境搭建:Webpack+Vue-router
 
-$ mpm install vue-cli -g
+$ npm install vue-cli -g
 $ vue init webpack pos
 $ npm install
+
 修改app组件,新建组件pos,修改路由文件index.js
+
+iconfont图标库,新建组件leftnav
+
+$ npm install element-ui --save
+main.js内引入,并全局Vue.use
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <router-view/>
+
+    <left-nav></left-nav>
+
+    <div class="main">
+      <router-view/>
+    </div>
+
+
   </div>
 </template>
 
 <script>
+import leftNav from '@/components/common/leftNav'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    leftNav
+  }
 }
 </script>
 
@@ -16,7 +27,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.main{
+  float: left;
+  width:95%;
+  background-color: #eff2f7;
+  height:100%;
+  overflow: hidden;
 }
 </style>
